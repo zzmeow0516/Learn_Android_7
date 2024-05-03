6c15bcb745f4c1f2a9f19dc917b5501784864d2a
zzmeow0516 <zzmeow0516@163.com>
2024-05-03 12:30:24 +0800

    Fragment lifecycle

--- app/src/main/java/com/example/learn_android_7/RightFragment.kt
+++ app/src/main/java/com/example/learn_android_7/RightFragment.kt
@@ -1,17 +1,63 @@
 package com.example.learn_android_7
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class RightFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+
+    //关联对象，用来创建一个与类关联而不需要创建实例的成员，这个成员可以直接通过类来访问
+    companion object {
+        const val TAG = "mylog_RightFragment"
+    }
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.d(TAG, "onAttach")
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate")
+    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        Log.d(TAG, "onCreateView")
         return inflater.inflate(R.layout.right_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d(TAG, "onViewCreated")
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d(TAG, "onDestroyView")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+    override fun onDetach() {
+        super.onDetach()
+        Log.d(TAG, "onDetach")
     }
 }
